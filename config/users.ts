7ccde934ceb8d82a2807d9d581f782f765ee4327
Damien Vesper <ldamienvesper@gmail.com>
2023-05-23 14:52:41 -0500

    e

--- config/users.ts
+++ config/users.ts
@@ -2,7 +2,7 @@ const users = {
     damienvesper: `386940319666667521`,
     killaship: `401849772157435905`,
     hasanger: `753029976474779760`,
-    katloo: `401849772157435905`
+    katloo: `643550424695308289`
 };
 
 export default users;
