b64faa90f6feb109174a6691b2e1d9077f17203a
Damien Vesper <ldamienvesper@gmail.com>
2024-08-06 13:17:38 -0500

    feat: remove command

---
+++ src/commands/music/Remove.ts
@@ -0,0 +1,46 @@
+import { SlashCommandBuilder, type ChatInputCommandInteraction } from 'discord.js';
+
+import { Command } from '../../classes/Command.js';
+
+class Remove extends Command {
+    cmd = new SlashCommandBuilder()
+        .setName(`remove`)
+        .addIntegerOption(option => option.setName(`id`).setDescription(`The position of the song in the queue.`).setMinValue(1).setRequired(true))
+        .setDescription(`Remove a song from the queue.`)
+        .setDMPermission(false);
+
+    run = async (interaction: ChatInputCommandInteraction): Promise<void> => {
+        if (interaction.guild === null) {
+            await interaction.reply({ content: `This command can only be used in a guild!`, ephemeral: true });
+            return;
+        }
+
+        const voiceChannel = (await interaction.guild.members.fetch(interaction.user.id)).voice.channel;
+        if (voiceChannel === null) {
+            await interaction.reply({ embeds: [this.client.createDenyEmbed(interaction.user, `You must be in a voice channel to use that command!`)], ephemeral: true });
+            return;
+        }
+
+        await interaction.deferReply();
+
+        const player = this.client.lavalinkManager.players.get(interaction.guild.id);
+        if (player === undefined) {
+            await interaction.followUp({ embeds: [this.client.createDenyEmbed(interaction.user, `I am not currently in a voice channel!`)] });
+            return;
+        } else if (player !== undefined && voiceChannel.id !== player.voiceChannel) {
+            await interaction.followUp({ embeds: [this.client.createDenyEmbed(interaction.user, `You must be in the same voice channel as the bot to use that command!`)] });
+            return;
+        }
+
+        const songID = interaction.options.getInteger(`id`, true);
+        if (songID > player.queue.length) {
+            await interaction.followUp({ embeds: [this.client.createDenyEmbed(interaction.user, `There are only **${player.queue.length}** songs in the queue!`)] });
+            return;
+        }
+
+        player.queue.splice(songID, songID + 1);
+        await interaction.followUp({ embeds: [this.client.createApproveEmbed(interaction.user, `Removed the song at position **${songID}** in the queue.`)] });
+    };
+}
+
+export default Remove;
