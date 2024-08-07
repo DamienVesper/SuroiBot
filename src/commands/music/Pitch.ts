dc12569c79a56cf919597b68ac2ef9ddfd2e9c46
Damien Vesper <ldamienvesper@gmail.com>
2024-08-07 11:44:52 -0500

    feat: pitch command

---
+++ src/commands/music/Pitch.ts
@@ -0,0 +1,41 @@
+import { SlashCommandBuilder, type ChatInputCommandInteraction } from 'discord.js';
+
+import { Command } from '../../classes/Command.js';
+
+class Pitch extends Command {
+    cmd = new SlashCommandBuilder()
+        .setName(`pitch`)
+        .addIntegerOption(option => option.setName(`value`).setDescription(`The pitch to set the audio to, as a percentage.`).setMinValue(1).setMaxValue(1e3))
+        .setDescription(`Set the pitch of the player.`)
+        .setDMPermission(false);
+
+    run = async (interaction: ChatInputCommandInteraction): Promise<void> => {
+        if (interaction.guild === null) {
+            await interaction.reply({ content: `This command can only be used in a guild!`, ephemeral: true });
+            return;
+        }
+
+        const voiceChannel = (await interaction.guild.members.fetch(interaction.user.id)).voice.channel;
+        if (voiceChannel === null) {
+            await interaction.reply({ embeds: [this.client.createDenyEmbed(interaction.user, `You must be in a voice channel to use that command!`)], ephemeral: true });
+            return;
+        }
+
+        const pitch = interaction.options.getInteger(`value`);
+        await interaction.deferReply();
+
+        const player = this.client.lavalink.players.get(interaction.guild.id);
+        if (player === undefined) {
+            await interaction.followUp({ embeds: [this.client.createDenyEmbed(interaction.user, `I am not currently in a voice channel!`)] });
+            return;
+        } else if (player !== undefined && voiceChannel.id !== player.voiceChannel) {
+            await interaction.followUp({ embeds: [this.client.createDenyEmbed(interaction.user, `You must be in the same voice channel as the bot to use that command!`)] });
+            return;
+        }
+
+        player.filters.setTimescale({ pitch: (pitch ?? 100) / 100 });
+        await interaction.followUp({ embeds: [this.client.createApproveEmbed(interaction.user, pitch !== null ? `Changed pitch by **${pitch}%**.` : `Reset the pitch.`)] });
+    };
+}
+
+export default Pitch;
