f3411e48a6ccc51824828257adc7347b8df83438
Damien Vesper <ldamienvesper@gmail.com>
2024-08-07 11:44:56 -0500

    feat: speed command

---
+++ src/commands/music/Speed.ts
@@ -0,0 +1,41 @@
+import { SlashCommandBuilder, type ChatInputCommandInteraction } from 'discord.js';
+
+import { Command } from '../../classes/Command.js';
+
+class Speed extends Command {
+    cmd = new SlashCommandBuilder()
+        .setName(`speed`)
+        .addIntegerOption(option => option.setName(`value`).setDescription(`The speed to set the audio to, as a multipler.`).setMinValue(1).setMaxValue(10))
+        .setDescription(`Set the speed of the player.`)
+        .setDMPermission(false);
+
+    run = async (interaction: ChatInputCommandInteraction): Promise<void> => {
+        if (interaction.guild === null) {
+            await interaction.reply({ content: `This command can only be used in a guild!`, ephemeral: true });
+            return;
+        }
+
+        const voiceChannel = (await interaction.guild.members.fetch(interaction.user.id)).voice.channel;
+        if (voiceChannel === null) {
+            await interaction.reply({ embeds: [this.client.createDenyEmbed(interaction.user, `You must be in a voice channel to use that command!`)], ephemeral: true });
+            return;
+        }
+
+        const speed = interaction.options.getInteger(`value`);
+        await interaction.deferReply();
+
+        const player = this.client.lavalink.players.get(interaction.guild.id);
+        if (player === undefined) {
+            await interaction.followUp({ embeds: [this.client.createDenyEmbed(interaction.user, `I am not currently in a voice channel!`)] });
+            return;
+        } else if (player !== undefined && voiceChannel.id !== player.voiceChannel) {
+            await interaction.followUp({ embeds: [this.client.createDenyEmbed(interaction.user, `You must be in the same voice channel as the bot to use that command!`)] });
+            return;
+        }
+
+        player.filters.setTimescale({ speed: speed ?? 1 });
+        await interaction.followUp({ embeds: [this.client.createApproveEmbed(interaction.user, speed !== null ? `Set the player speed to **${speed}x**.` : `Reset the player speed.`)] });
+    };
+}
+
+export default Speed;
