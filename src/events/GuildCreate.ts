44c45f5834a49e09d3bee58cd594ab03c114ea42
Damien Vesper <ldamienvesper@gmail.com>
2024-09-02 14:56:37 -0500

    chore: bump deps

---
+++ src/events/GuildCreate.ts
@@ -0,0 +1,30 @@
+import { Events } from 'discord.js';
+
+import { Event } from '../classes/Event.js';
+
+const EventType = Events.GuildCreate;
+
+class GuildCreate extends Event<typeof EventType> {
+    constructor (client: Event<typeof EventType>[`client`]) {
+        super(client);
+
+        this.config = {
+            name: EventType,
+            once: false
+        };
+
+        this.run = async guild => {
+            const guildDoc = await this.client.db.guild.findUnique({ where: { discordId: guild.id } });
+            if (guildDoc === null) {
+                this.client.logger.info(`Database`, `Created entry for guild "${guild.name}" (${guild.id}).`);
+                await this.client.db.guild.create({
+                    data: {
+                        discordId: guild.id
+                    }
+                });
+            }
+        };
+    }
+}
+
+export default GuildCreate;
--- src/events/MessageCreate.ts
+++ src/events/MessageCreate.ts
@@ -16,19 +16,64 @@ class MessageCreate extends Event<typeof EventType> {
         this.run = async message => {
             if (message.author.bot || message.guild === null) return;
 
-            if (this.client.config.modules.leveling.enabled) {
-                const dbUser = await this.client.db.user.findUnique({ where: { discordId: message.author.id } });
-                if (dbUser === null) {
-                    this.client.logger.info(`Database`, `Created account for "${message.author.tag}" (${message.author.id}) in "${message.guild.name}" (${message.guild.id}).`);
-                    await this.client.db.user.create({
-                        data: {
-                            discordId: message.author.id,
-                            guildId: message.guild.id,
-                            level: this.client.config.modules.leveling.level.min
+            let dbUser = await this.client.db.user.findUnique({
+                where: {
+                    discordId: message.author.id,
+                    guildId: message.guild.id
+                },
+                include: {
+                    cooldowns: true
+                }
+            });
+
+            let guild = await this.client.db.guild.findUnique({ where: { discordId: message.guild.id } });
+
+            if (guild === null) {
+                this.client.logger.info(`Database`, `Created entry for guild "${message.guild.name}" (${message.guild.id}).`);
+                guild = await this.client.db.guild.create({
+                    data: {
+                        discordId: message.guild.id
+                    }
+                });
+            }
+
+            if (dbUser === null) {
+                this.client.logger.info(`Database`, `Created account for "${message.author.tag}" (${message.author.id}) in "${message.guild.name}" (${message.guild.id}).`);
+                await this.client.db.user.create({
+                    data: {
+                        discordId: message.author.id,
+                        guildId: message.guild.id,
+                        level: this.client.config.modules.leveling.enabled ? this.client.config.modules.leveling.level.min : 0,
+                        cooldowns: {
+                            create: {
+                                daily: new Date(0),
+                                xp: new Date(0)
+                            }
                         }
-                    });
-                } else {
-                    // if (dbUser.cooldowns)
+                    }
+                });
+
+                dbUser = await this.client.db.user.findUnique({
+                    where: {
+                        discordId: message.author.id,
+                        guildId: message.guild.id
+                    },
+                    include: {
+                        cooldowns: true
+                    }
+                });
+            }
+
+            if (this.client.config.modules.leveling.enabled) {
+                if (guild === null || dbUser === null) {
+                    this.client.logger.error(`Database`, `Guild or User were found null for "${message.author.tag}" (${message.author.id}) in "${message.guild.name}" (${message.guild.id}).`);
+                    return;
+                }
+
+                console.log(dbUser.cooldowns);
+                if ((new Date().valueOf() - (dbUser.cooldowns.xp ?? new Date(0)).valueOf()) < this.client.config.modules.leveling.xpCooldown) {
+                    dbUser.xp += Math.floor(Math.random() * this.client.config.modules.leveling.xp.max + this.client.config.modules.leveling.xp.min);
+                    dbUser.cooldowns.xp = new Date();
                 }
             }
         };
